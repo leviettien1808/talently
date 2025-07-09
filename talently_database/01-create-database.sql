27ae7131501c7cf50c520ccdff29489b9074f05e
leviettien1808 <leviettien1808@gmail.com>
2025-07-09 09:40:14 +0700

    Initialize database

---
+++ talently_database/01-create-database.sql
@@ -0,0 +1,15 @@
+-------------------- CREATE DATABASE --------------------
+-- CREATE DATABASE IF NOT EXISTS talently; -- MySQL
+-- CREATE SCHEMA talently; -- MySQL: this command is the same as CREATE DATABASE
+CREATE DATABASE talently; -- MySQL/PostgreSQL
+-- sqlite3 talently.db -- SQLite: this command is run in the terminal to create a database file
+
+
+-- Reference MySQL: https://dev.mysql.com/doc/refman/8.4/en/create-database.html
+-- Reference PostgreSQL: https://www.postgresql.org/docs/current/sql-createdatabase.html
+-- Reference SQLite: https://www.sqlite.org/cli.html#open
+
+
+
+-------------------- DELETE DATABASE --------------------
+-- DROP DATABASE talently; -- MySQL/PostgreSQL
---
+++ talently_database/README.md
@@ -0,0 +1,10 @@
+# 💥Summary
+A fiction project about talent company
+
+## SQL
+I used Data Definition Language (DDL) and Data Manipulation Language (DML) for three Database Management System
+- Create database
+### MySQL
+### Postgres
+### SQLite
+- Create database: .open D:\programming\projects\talently\talently_database\talently.db
